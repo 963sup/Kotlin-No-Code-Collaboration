efdf7b62d40641d4cf4a13dee1248f6801cf7d5f
963sup <963sup@gmail.com>
2026-08-18 18:43:49 +0800

    Test enterprise backup policy

---
+++ app/src/test/java/com/example/BackupPolicyTest.kt
@@ -0,0 +1,21 @@
+package com.example
+
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [36])
+class BackupPolicyTest {
+    @Test
+    fun enterpriseCollaborationDataIsNotEligibleForAutoBackup() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val allowBackupFlag = context.applicationInfo.flags and ApplicationInfo.FLAG_ALLOW_BACKUP
+        assertEquals(0, allowBackupFlag)
+    }
+}
